c15884b40b83d1ac15b92440c8ac4a513da607a6
yz.coding@gmail.com <Young.Zhang_CTR@intersystems.com>
2017-02-24 14:34:01 -0500

    initial check in

---
+++ src/app.js
@@ -0,0 +1,2 @@
+
+var homeModule = require('./home');
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    inline: true
+  }
+};
